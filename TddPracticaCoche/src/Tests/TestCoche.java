e4316e834f1635ece3bd59cc1d49f48ed2ec43e8
Sandra Sáez Ibarra <sansaeiba@alu.edu.gva.es>
2023-04-02 18:01:21 +0200

    Primer test y clase coche

--- TddPracticaCoche/src/Tests/TestCoche.java
+++ TddPracticaCoche/src/Tests/TestCoche.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 class TestCoche {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_crear_coche() {
+		Coche nuevoCoche = new Coche();
 	}
 
 }
